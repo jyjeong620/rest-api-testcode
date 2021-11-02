9c8aa9007a020a2cfb23ed80357620c28de4bffd
정준영 <jeongjy@mrblue.com>
2021-11-02 11:29:43 +0900

    Initial commit

---
+++ src/main/java/jeongjy/restapitestcode/RestApiTestcodeApplication.java
@@ -0,0 +1,13 @@
+package jeongjy.restapitestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestcodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestApiTestcodeApplication.class, args);
+  }
+
+}
